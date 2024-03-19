7b568577ae98d59c125cad46510d79fcbc226ba2
Lucile THIEULIN <lucile.thieulin96@gmail.com>
2024-03-19 17:23:35 +0100

    remise en place du scroll

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import { useTranslation } from 'react-i18next';
-import Header from '../components/header';
 
 // scroll horizontal
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 import { gsap } from 'gsap/dist/gsap';
 import { useEffect } from 'react';
-import About from '../components/About';
+import Header from '../components/header';
 
 gsap.registerPlugin(ScrollTrigger);
+
+//composants
 
 export default function Home() {
   // i18n
@@ -31,33 +32,20 @@ export default function Home() {
     });
   }, []);
   return (
-    <>
-      <div className="container bg-painterGreyLight/50 dark:bg-painterDark">
-        {' '}
-        <section className="panel flex flex-col">
-          <Header />
-          <p>{t('hourly')}</p>
-          <h1>SCROLL DOWN</h1>
-        </section>
-        <section className="panel">
-          <About />
-        </section>
-        <section className="panel">
-          <h2>TWO</h2>
-        </section>
-        <section className="panel">
-          <h2>THREE</h2>
-        </section>
-        <section className="panel">
-          <h2>FOUR</h2>
-        </section>
-        <section className="panel">
-          <h2>FIVE</h2>
-        </section>
-        <section className="panel">
-          <h2>FOOTER</h2>
-        </section>
-      </div>
-    </>
+    <div className="container bg-gray-400">
+      <section className="panel">
+        <Header />
+        <h1>SCROLL DOWN</h1>
+      </section>
+      <section className="panel">
+        <h2>ONE</h2>
+      </section>
+      <section className="panel">
+        <h2>TWO</h2>
+      </section>
+      <section className="panel">
+        <h2>THREE</h2>
+      </section>
+    </div>
   );
 }
