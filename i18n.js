d623f327faf397b959cfa2325df3ff34482b02b4
Lucile THIEULIN <lucile.thieulin96@gmail.com>
2024-03-19 16:56:54 +0100

    ajout traduction about

--- i18n.js
+++ i18n.js
@@ -1,20 +1,22 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import aboutTranslationEN from './public/locales/en/about.json';
 import headerTranslationsEN from './public/locales/en/header.json';
-import headerTranslationsFR from './public/locales/fr/header.json';
 import navbarTranslationEN from './public/locales/en/navbar.json';
+import aboutTranslationFR from './public/locales/fr/about.json';
+import headerTranslationsFR from './public/locales/fr/header.json';
 import navbarTranslationFR from './public/locales/fr/navbar.json';
-
-
 
 const resources = {
   en: {
     header: headerTranslationsEN,
     navbar: navbarTranslationEN,
+    about: aboutTranslationEN
   },
   fr: {
     header: headerTranslationsFR,
     navbar: navbarTranslationFR,
+    aboutTranslationFR
   }
 };
 
